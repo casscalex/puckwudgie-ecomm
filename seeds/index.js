ac99de860662a0e94164731a14cd773542d3523a
samini1 <92944248+samini1@users.noreply.github.com>
2022-02-15 19:22:17 -0600

    fix to merger

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,7 @@ const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: false });
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
