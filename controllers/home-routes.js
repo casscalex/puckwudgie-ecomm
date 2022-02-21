e265fc162c2953638050d1314efae35eb338a78d
samini1 <92944248+samini1@users.noreply.github.com>
2022-02-21 11:34:24 -0600

    decided to exchange dashboard with shopping cart added store routes in home routes moved storefront to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Product, Category, User } = require('../models');
-const withAuth = require('../utils/auth');
 
 // added withAuth requierment
 router.get('/', (req, res) => {
@@ -29,6 +28,38 @@ router.get('/', (req, res) => {
   .then(dbProductData => {
     const products = dbProductData.map(product => product.get({ plain: true }));
     res.render('homepage', { products, loggedIn: req.session.loggedIn });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.get('/store', (req, res) => {
+  console.log(req.session);
+  Product.findAll({
+    attributes: [
+      "id", 
+      "product_name", 
+      "description", 
+      "img_url",
+      "price",
+      "stock",
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['id', 'username'],
+      },
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      }
+    ]
+  })
+  .then(dbProductData => {
+    const products = dbProductData.map(product => product.get({ plain: true }));
+    res.render('store', { products, loggedIn: req.session.loggedIn });
   })
   .catch(err => {
     console.log(err);
